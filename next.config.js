632f7add40648f2d4ca53016f96dd9e2c8a85040
Esau Jesse <jesseesau13@gmail.com>
2025-07-17 10:24:27 +0100

    Update next.config.js and package.json

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/learnkore',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  trailSlash: true,
   reactStrictMode: true,
 }
 
